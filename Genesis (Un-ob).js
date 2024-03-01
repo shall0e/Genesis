c5e13657f10fe0c433a675dd33362cbd00e12784
Shallow <100087924+Shall0e@users.noreply.github.com>
2024-03-01 09:24:18 -0500

    Update Genesis (Un-ob).js

--- Genesis (Un-ob).js
+++ Genesis (Un-ob).js
@@ -1,6 +1,8 @@
 const genesis = {}
 genesis.scratch = document.getElementById('app')._reactRootContainer._internalRoot.current.child.pendingProps.store.getState()
 genesis.genesis = document.getElementById('app')._reactRootContainer._internalRoot.current.child.pendingProps.store.getState().scratchGui.vm.runtime.getSpriteTargetByName('Genesis')
+genesis.scratch.session.session.user.token = "tokenHidden";
+genesis.scratch.session.session.user.email = "email@email.com";
 genesis.filterText = [
   'blowing up computer in t-minus 3 seconds.',
   "don't spread viruses, spread love!",
@@ -9,17 +11,17 @@ genesis.filterText = [
   "using Genesis to steal accounts is wrong, the creators project has been logged to the Genesis Database.",
   "contact the creator and ask them why they are trying to steal accounts.",
   "don't abuse Genesis, we block webhooks and sending data."
-]
+];
 genesis.getVariable = function(target,variable){
   if (target == 'stage') {
-    return genesis.scratch.scratchGui.vm.runtime.getTargetForStage().lookupVariableByNameAndType(String(variable),'').value
+    return genesis.scratch.scratchGui.vm.runtime.getTargetForStage().lookupVariableByNameAndType(String(variable),'').value;
   } else {
-    return genesis.scratch.scratchGui.vm.runtime.getSpriteTargetByName(String(target)).lookupVariableByNameAndType(String(variable),'').value
+    return genesis.scratch.scratchGui.vm.runtime.getSpriteTargetByName(String(target)).lookupVariableByNameAndType(String(variable),'').value;
   }
-}
+};
 var oldOpcode = 9999
 var evalReturn;
-function b(e){let o="";for(let t=0;t<e.length;t++)"A"<=e[t]&&e[t]<="Z"?o+=String.fromCharCode(e.charCodeAt(t)+32):o+=e[t];return o}
+function b(e){let o="";for(let t=0;t<e.length;t++)"A"<=e[t]&&e[t]<="Z"?o+=String.fromCharCode(e.charCodeAt(t)+32):o+=e[t];return o};
 function a(){
   setInterval(function() {
     genesis.genesis.lookupVariableByNameAndType('connected?', '').value = 'connected';
@@ -34,7 +36,7 @@ function a(){
         } else {
           genesis.genesis.lookupVariableByNameAndType('return', '').value = eval(evalReturn)
         }
-      }
-    }; oldOpcode = genesis.genesis.lookupVariableByNameAndType('opcode', '').value
+      }; oldOpcode = genesis.genesis.lookupVariableByNameAndType('opcode', '').value; genesis.genesis.lookupVariableByNameAndType('oldOpcode', '').value = genesis.genesis.lookupVariableByNameAndType('opcode', '').value;
+    }
   }, 33)
-};a()
+};a();
